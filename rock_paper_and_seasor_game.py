df65878cf32cdbe5e2ba83245dd1ef95f5575be0
skchhotarayskc <88996519+skchhotarayskc@users.noreply.github.com>
2022-05-18 02:33:48 +0530

    Add files via upload

---
+++ rock_paper_and_seasor_game.py
@@ -0,0 +1,78 @@
+import random
+
+rock = ['''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+''']
+
+paper = ['''
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+''']
+
+seasor = ['''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+''']
+
+print("Welcome to rock, paper, and seasor game!")
+
+choice = int(input("What do you choose? Type 1 for Rock, 2 for paper or 3 for seasor : "))
+computer_choice = random.randint(1, 3)
+
+if (choice == 1 and computer_choice == 1) or (choice == 2 and computer_choice == 2) or (choice == 3 and computer_choice == 3):
+    print("Match draw")
+
+elif choice >= 4 or choice <= 0:
+    print("You entered an invalid number. You lose")
+    
+else:
+    if choice == 1 and computer_choice == 2 :
+        print("You choose rock.")
+        print(rock[0])
+        print("computer choose paper.")
+        print(paper[0])
+        print("You lose!")
+    elif choice == 1 and computer_choice == 3 :
+        print("You choose rock.")
+        print(rock[0])
+        print("computer choose seasor.")
+        print(seasor[0])
+        print("You won!")
+
+    elif choice == 2 and computer_choice == 1 :
+        print("You choose paper.")
+        print(paper[0])
+        print("computer choose rock.")
+        print(rock[0])
+        print("You won!")
+    elif choice == 2 and computer_choice == 3 :
+        print("You choose paper.")
+        print(paper[0])
+        print("computer choose seasor.")
+        print(seasor[0])
+        print("You lose!")
+    
+    elif choice == 3 and computer_choice == 1 :
+        print("You choose seasor.")
+        print(seasor[0])
+        print("computer choose rock.")
+        print("You lose!")
+    elif choice == 3 and computer_choice == 2 :
+        print("You choose seasor.")
+        print(seasor[0])
+        print("computer choose paper.")
+        print(paper[0])
+        print("You won!")
